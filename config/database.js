9f8dc4b43b6ea492c55b0ec6baf7f73cf5b70d58
SrGokuto <jeonardoelfaker@gmail.com>
2026-06-12 17:33:40 -0500

    fix typos

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 // Crear pool de conexiones
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3305,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'task_manager',
